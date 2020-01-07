9e456df8d7a53b7b9e0abf2e0a0a86174001e53d
Your Name <khorenterhovhannisyan@gmail.com>
2020-01-07 20:43:23 +0400

    Updates

---
+++ precache-manifest.25ce7d557fee9998e97e001ea4bdd322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729b4edae336107ac5e348678712378f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb27603b452471e194a",
+    "url": "/static/css/main.c0b047c5.chunk.css"
+  },
+  {
+    "revision": "86674c6ba4db9d4f7010",
+    "url": "/static/js/2.93591f3f.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.93591f3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acb27603b452471e194a",
+    "url": "/static/js/main.2a5ef42c.chunk.js"
+  },
+  {
+    "revision": "4893ee31284d2623d244",
+    "url": "/static/js/runtime-main.addc1197.js"
+  }
+]);
